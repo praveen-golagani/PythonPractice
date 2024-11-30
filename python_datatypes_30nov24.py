9b92ca28867d501093e267ca6ad348334c4e2709
praveen-golagani <praveen.gyj@gmail.com>
2024-11-30 23:51:32 +0530

    tuple and list data types prac

---
+++ python_datatypes_30nov24.py
@@ -0,0 +1,16 @@
+num_var = 123987
+name_var = "praveen"
+deci_var = 12.34
+t_boolean_var = True
+f_boolean_var = False
+
+#list allows multiple data type
+list_items = ['Chocolate',"Apple",235,"dollars",'a',34.65]
+print(list_items)
+print(list_items[5])
+
+#tuple allows multiple
+
+list_items = ('Chocolate',"Apple",235,"dollars",'a',34.65)
+print(list_items)
+print(list_items[5])
