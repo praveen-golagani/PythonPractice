d1dcb2e36f2f9ba8609574ec069e21f09c5275ef
praveen-golagani <praveen.gyj@gmail.com>
2024-11-30 23:38:26 +0530

    immutable data types test

---
+++ python_immutable_30nov24.py
@@ -0,0 +1,26 @@
+#print("restart the phase")
+print("welcome")
+
+# immutable
+
+tuple_names = ("praveen",22,"code")
+
+print(tuple_names[1])
+##tuple_names[1]="pk"
+##print(tuple_names[1])
+
+college = "Jntukkd"
+print(college[6])
+
+##college[3]="S"
+##print(college[3])
+
+#mutable 
+dict_students = {"roll_1": 2322,"name":"praveen"}
+print(dict_students["name"])
+
+list_students = ["prav",23,"ravindra","sachin"]
+list_students[1]="sehwag"
+
+print(list_students[1])
+print(list_students)
