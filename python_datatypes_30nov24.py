a901246fd4fc71dd5d0d2c2e8e25bf5fd2b1b624
praveen-golagani <praveen.gyj@gmail.com>
2024-12-01 09:56:26 +0530

    data types and their type

--- python_datatypes_30nov24.py
+++ python_datatypes_30nov24.py
@@ -8,9 +8,35 @@ f_boolean_var = False
 list_items = ['Chocolate',"Apple",235,"dollars",'a',34.65]
 print(list_items)
 print(list_items[5])
+print(type(list_items))
 
 #tuple allows multiple
 
-list_items = ('Chocolate',"Apple",235,"dollars",'a',34.65)
-print(list_items)
-print(list_items[5])
+tuple_items = ('Chocolate',"Apple",235,"dollars",'a',34.65)
+print(tuple_items)
+print(tuple_items[5])
+
+print(type(tuple_items))
+
+#dictionary
+
+dict_cart = {"item region":"usa", "item name" : "car", "item number":2965}
+print(dict_cart["item name"])
+
+#type
+
+str_var = "healthy"
+tuple_cart = ("healthy")
+tuple_cart1 = ("healthy",)
+print(f"the data type of {num_var} is {type(num_var)}")
+print(f"the data type of {deci_var} is {type(deci_var)}")
+      
+print(f"the data type of {str_var} is {type(str_var)}")
+#observe diff
+print(tuple_cart)
+print(f"the data type of tuple_cart is {type(tuple_cart)}")
+print(tuple_cart1)
+print(f"the data type of tuple_cart1 is {type(tuple_cart1)}")
+
+
+
