58cf4347dbd91442892d51da4d155b4db5dc46b8
praveen-golagani <praveen.gyj@gmail.com>
2024-12-05 07:59:30 +0530

    weekday program

---
+++ AllPrograms/range_func_5dec24.py
@@ -0,0 +1,8 @@
+#range()
+#print values b/w range
+print(range(10))
+for i in range(10):
+    print(i)            #prints 0 to 9
+
+for j in range(1,10):
+    print(j)
--- AllPrograms/weekday_4dec24.py
+++ AllPrograms/weekday_4dec24.py
@@ -1,5 +1,11 @@
 weekday_name = input("Enter day of the week : ").lower()
 # dictionary with the days of week
-dict_week = {"sunday": 1, "monday": 2, "tuesday": 3, "wednesday": 4, "thursday": 5, "friday": 6, "saturday": 7}
+dict_week = {"sunday": 1,
+             "monday": 2,
+             "tuesday": 3,
+             "wednesday": 4,
+             "thursday": 5,
+             "friday": 6,
+             "saturday": 7}
 
 print(f"day number : {dict_week[weekday_name]}")
