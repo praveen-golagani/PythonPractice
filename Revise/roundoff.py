8d1d7ea565ae9b508ac30196da4f868daaae427e
praveen-golagani <praveen.gyj@gmail.com>
2024-12-10 11:46:20 +0530

    round off

---
+++ Revise/roundoff.py
@@ -0,0 +1,14 @@
+pass_percent = 95.56
+weight_grams = 125.4356
+win_percent = 95.41
+draw_percent = 0.65
+rank = 1
+
+def round_two_dec(a):
+    print(f"{a:.2f}")
+    
+def round_one_dec(a):
+    print(f"{a:.1f}")
+round_two_dec(weight_grams)
+round_one_dec(weight_grams)
+round_two_dec(rank)
