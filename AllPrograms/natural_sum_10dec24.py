8671f858813343e8cc5ff5cd160f4b73bf0bfff7
praveen-golagani <praveen.gyj@gmail.com>
2024-12-11 23:36:23 +0530

    sum of natural

--- AllPrograms/natural_sum_10dec24.py
+++ AllPrograms/natural_sum_10dec24.py
@@ -7,3 +7,9 @@ while i<=natural_num:
     i+=1   #increment to avoid infinite loop
     
 print(sum)
+
+#using for loop
+f_result = 0
+for j in range(1,natural_num+1):
+    f_result+=j
+print(f_result)
